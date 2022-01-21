083a9a16b8d8d1fe08ee791cfa01b6921e759bca
CarlosBocarando <95479787+CarlosBocarando@users.noreply.github.com>
2022-01-21 12:42:07 +0100

    Update PruebasGit2.java
    Añadido comentario en GitHUb

--- Pruebas-Git2/src/pruebas/git2/PruebasGit2.java
+++ Pruebas-Git2/src/pruebas/git2/PruebasGit2.java
@@ -15,6 +15,7 @@ public class PruebasGit2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Prueba de comentario
         System.out.println("Hola Mundo");
     }
     
